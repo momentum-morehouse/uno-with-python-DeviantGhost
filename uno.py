9f0d1fbbaeec75604d915bd5de8253b30b17f14a
DeviantGhost <carlonjwork@gmail.com>
2020-10-12 15:44:30 -0500

    Debug required

--- uno.py
+++ uno.py
@@ -2,8 +2,8 @@ from random import randint
 
 NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 COLORS = ["🔴","🟢","🟡","🔵"]
-PLAYERS = []
-PLAYER_REF = []
+PLAYER_REF = {}
+
 class Card:
     def __init__(self, number, color):
         self.number = number
@@ -18,7 +18,6 @@ class Player:
 
         self.name = name
         self.hand = hand
-        PLAYERS.append(name)
 
 
 class Deck:
@@ -26,7 +25,7 @@ class Deck:
         self.cards = []
         for number in numbers:
             for color in colors:
-                card = (color, number)
+                card = Card(number, color)
                 self.cards.append(card)
 
     def shuffle(self):
@@ -64,37 +63,48 @@ class Game:
 
 
     def deal_start_hand(self, number_of_players,deck):
-        for current_player in PLAYERS:
-            temp_deck = deck
-            while (len(PLAYER_REF[PLAYERS.index(current_player)].hand) < 7):
-                PLAYER_REF[PLAYERS.index(current_player)].hand.append(temp_deck[0])
-                deck.remove(temp_deck[0])    
-        return temp_deck
+        players = list(PLAYER_REF.values())
+        for i in range(7):
+            for current_player in players:
+                print(len(current_player.hand))
+                print("current_player", current_player)
+                card = deck.pop()
+                current_player.hand = current_player.hand.extend(card)
+                print(len(deck))
+            i += 1
+        # print(PLAYER_REF.values())
+        # card = deck[0]
+        # print(card)
+        # deck.remove(deck[0])
+        # print(len(deck))
+        # for player in PLAYER_REF.values():
+            
+        return deck
 
 def startGame():
     #Creates the game
     game = Game()
-
     #Gets deck
     deck = game.shuffled_deck
-   
     #Creates the players with an empty hand
     enough_players = False
     while (enough_players == False):
-        
-        PLAYER_REF.append(Player(input(f"What is player {len(PLAYERS)+1}'s name: ")))
-        
-        if(len(PLAYERS) < 2):
-            PLAYER_REF.append(Player(input(f"What is player {len(PLAYERS)+1}'s name: ")))
+
+        newPlayer = Player(input(f"What is player {len(PLAYER_REF)+1}'s name: "))
+        PLAYER_REF[newPlayer.name] = newPlayer
+
+        if(len(PLAYER_REF) < 2):
+            newPlayer = Player(input(f"What is player {len(PLAYER_REF)+1}'s name: "))
+            PLAYER_REF[newPlayer.name] = newPlayer
             
         more_players = input("Would you like to add more players? (y/n)")
         if (more_players == "n"):
             enough_players = True
     #Assigns starting hand to each player
-    deck = game.deal_start_hand(len(PLAYERS),deck)
-    print (PLAYER_REF[0].hand)
-    print (PLAYER_REF[1].hand)
+    deck = game.deal_start_hand(len(PLAYER_REF),deck)
 
+    for key,value in PLAYER_REF.items():
+        print(f"{key}'s hand = {value.hand}")
     #Starts the game of/Creates a card pile
     #card_Pile = Card_Pile(deck)
     
